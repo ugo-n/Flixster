3c1acfee939601230091d51b7a67eb1cd0e18078
ugo-n <eagle.nnakwe@gmail.com>
2022-02-25 17:15:34 -0500

    Add walkthrough

--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -40,7 +40,7 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
-        private val ivBackdrop = itemView.findViewById<ImageView>(R.id.ivPoster)
+        //private val ivBackdrop = itemView.findViewById<ImageView>(R.id.ivPoster)
 
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
