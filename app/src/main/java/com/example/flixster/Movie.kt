f9af82acb546f7b6b30689cb7bd4a9d3dfdb91a3
ugo-n <eagle.nnakwe@gmail.com>
2022-02-25 15:40:48 -0500

    Implemented landscape mode

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -6,10 +6,12 @@ import org.json.JSONArray
 data class Movie (
     val movieId: Int,
     private val posterPath: String,
+    private val backdropPath: String,
     val title: String,
     val overview: String,
 ) {
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+    val backdropPathUrl = "https://image.tmdb.org/t/p/w780/$backdropPath"
     companion object{
         //allows us to call methods on the movie class without having an instance
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
@@ -20,6 +22,7 @@ data class Movie (
                     Movie(
                         movieJson.getInt("id"),
                         movieJson.getString("poster_path"),
+                        movieJson.getString("backdrop_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview")
                     )
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,7 +1,7 @@
 package com.example.flixster
 
 import android.content.Context
-import android.text.Layout
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,16 +10,21 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+private const val TAG =  "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
 
+    //Expensive operation: create a view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        Log.i(TAG, "onCreateViewHolder")
         val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
         return ViewHolder(view)
     }
 
+    //Cheap: simply bind data to an existing viewholder
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.i(TAG, "onBindViewHolder position $position")
         val movie = movies[position]
         holder.bind(movie)
     }
@@ -29,14 +34,16 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-        private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
+        private val ivPoster = itemView.findViewById<ImageView>(R.id.ivBackdrop)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+        private val ivBackdrop = itemView.findViewById<ImageView>(R.id.ivBackdrop)
 
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+            Glide.with(context).load(movie.backdropPathUrl).into(ivBackdrop)
         }
     }
 
