9afef12e9afaea47528b568864bc3befc29eec66
ugo-n <eagle.nnakwe@gmail.com>
2022-02-25 13:58:27 -0500

    Added Movie.kt data class

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -6,9 +6,13 @@ import android.util.Log
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+
 private const val TAG= "MainActivity"
 private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 class MainActivity : AppCompatActivity() {
+
+    private val movies = mutableListOf<Movie>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,8 +25,11 @@ class MainActivity : AppCompatActivity() {
                 Log.e(TAG, "onFailure $statusCode")
             }
 
-            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
                 Log.i(TAG, "onSuccess: JSON data $json")
+                val movieJsonArray = json.jsonObject.getJSONArray("results")
+                movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                Log.i(TAG, "Movie List $movies")
             }
 
         })
